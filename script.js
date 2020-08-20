df540b6b3417f63d6225d560d6cd61d5ab8f5e95
William Henderson <william-henderson@outlook.com>
2020-08-20 12:20:02 +0100

    Added GCSE qualifications

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var acceptedKeys = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ._").split("").concat(["Enter","Backspace"]);
+var acceptedKeys = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ._").split("").concat(["Enter", "Backspace"]);
 
 function mobileAndTabletCheck() { // from http://detectmobilebrowsers.com/
   let check = false;
-  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
   return check;
 };
 
@@ -43,7 +43,7 @@ async function intro() {
       await sleep(25);
     }
     document.getElementById("consoleArea").className = "animated";
-    await sleep(Math.random()*2000+1);
+    await sleep(Math.random() * 2000 + 1);
     cmd.innerHTML += " Done<br>";
   }
   await sleep(250);
@@ -61,7 +61,7 @@ async function intro() {
 function exitCommand() {
   document.getElementById("consoleHolder").className = "console goodbyeAnimation";
   document.getElementById("exitImage").className = "exitImage finalAnimation";
-  window.setTimeout(function(){document.body.innerHTML="";},300);
+  window.setTimeout(function () { document.body.innerHTML = ""; }, 300);
 }
 
 function clearCommand() {
@@ -80,7 +80,7 @@ QUALIFICATIONS.EXE&nbsp;&nbsp;CONTACT.EXE<br>`;
 }
 
 function echoCommand(toEcho) {
-  document.getElementById("consoleArea").innerHTML += "<br>"+toEcho+"<br>";
+  document.getElementById("consoleArea").innerHTML += "<br>" + toEcho + "<br>";
   document.getElementById("consoleArea").innerHTML += "<br><span class=\"dollar\">$ </span>";
 }
 
@@ -100,8 +100,13 @@ function contactCommand() {
 }
 
 function qualificationsCommand() {
-  document.getElementById("consoleArea").innerHTML += `<br>
-  ERROR: no qualifications found - fingers crossed for 20th August!
+  document.getElementById("consoleArea").innerHTML += `<br><br>
+  10 Level 9's at GCSE:<br><br>
+Mathematics: 9&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;English Literature: 9<br>
+English Language: 9&nbsp;&nbsp;&nbsp;&nbsp;Physics: 9<br>
+Chemistry: 9&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Biology: 9<br>
+Computer Science: 9&nbsp;&nbsp;&nbsp;&nbsp;Music: 9<br>
+French: 9&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Religion: 9
   <br><br><span class=\"dollar\">$ </span>`;
 }
 
@@ -109,8 +114,8 @@ function inputEvent(e) {
   if (acceptedKeys.includes(e.key) && introDone) {
     if (e.key == "Enter") {
       var commands = document.getElementById("consoleArea").innerHTML.split("$ ");
-      var command = commands[commands.length-1].replace("</span>","").toLowerCase().replace(".exe","");
-      console.log("'"+command+"'");
+      var command = commands[commands.length - 1].replace("</span>", "").toLowerCase().replace(".exe", "");
+      console.log("'" + command + "'");
       if (command == "exit" || command == "quit" || command == "goodbye" || command == "shutdown") {
         exitCommand();
       } else if (command == "clear" || command == "cls") {
@@ -141,9 +146,9 @@ function inputEvent(e) {
         qualificationsCommand();
       } else if (command == "contact") {
         contactCommand();
-      } else if (command.slice(0,5) == "echo ") {
+      } else if (command.slice(0, 5) == "echo ") {
         echoCommand(command.slice(5))
-      } else if (command.substring(0,3) == "cd ") {
+      } else if (command.substring(0, 3) == "cd ") {
         document.getElementById("consoleArea").innerHTML += "<br>There are no other directories on this entire system, so you'll have to stay here.<br><br><span class=\"dollar\">$ </span>";
       } else if (command == "help") {
         document.getElementById("consoleArea").innerHTML += "<br>The 'dir' command might be a good place to start.<br><br><span class=\"dollar\">$ </span>";
@@ -152,14 +157,14 @@ function inputEvent(e) {
       } else if (command == "") {
         document.getElementById("consoleArea").innerHTML += "<br><span class=\"dollar\">$ </span>";
       } else {
-        document.getElementById("consoleArea").innerHTML += "<br>'"+command+"' is not recognised as an internal or external command, operable program or batch file.<br><br><span class=\"dollar\">$ </span>";
+        document.getElementById("consoleArea").innerHTML += "<br>'" + command + "' is not recognised as an internal or external command, operable program or batch file.<br><br><span class=\"dollar\">$ </span>";
       }
       var dollars = document.getElementsByClassName("dollar");
-      dollars[dollars.length-1].scrollIntoView();
+      dollars[dollars.length - 1].scrollIntoView();
     } else if (e.key == "Backspace") {
       console.log(document.getElementById("consoleArea").innerHTML.slice(-2));
       if (document.getElementById("consoleArea").innerHTML.slice(-2) != "$ ") {
-        document.getElementById("consoleArea").innerHTML = document.getElementById("consoleArea").innerHTML.slice(0,-1);
+        document.getElementById("consoleArea").innerHTML = document.getElementById("consoleArea").innerHTML.slice(0, -1);
       }
     } else {
       document.getElementById("consoleArea").innerHTML += e.key;
@@ -179,18 +184,18 @@ function clickToClose(e) {
     e.target.className = "normalWindow";
     document.getElementById("consoleArea").innerHTML += "<br><span class=\"dollar\">$ </span>";
     var dollars = document.getElementsByClassName("dollar");
-    dollars[dollars.length-1].scrollIntoView();
+    dollars[dollars.length - 1].scrollIntoView();
     introDone = true;
-    document.removeEventListener("click",clickToClose);
-    slideshows[e.target.id.replace("Window","")].count = -1;
-    window.setTimeout(function(target=e.target){
-      document.getElementById(target.id.replace("Window","Content")).innerHTML = slideshows[target.id.replace("Window","")].originalContent;
-    },500);
+    document.removeEventListener("click", clickToClose);
+    slideshows[e.target.id.replace("Window", "")].count = -1;
+    window.setTimeout(function (target = e.target) {
+      document.getElementById(target.id.replace("Window", "Content")).innerHTML = slideshows[target.id.replace("Window", "")].originalContent;
+    }, 500);
   }
 }
 
 function openExtraWindow() {
-  document.addEventListener("click",clickToClose);
+  document.addEventListener("click", clickToClose);
 }
 
 function slideshowNext(slideshow) {
@@ -199,19 +204,19 @@ function slideshowNext(slideshow) {
     document.getElementsByClassName("separateWindowActive")[0].className = "normalWindow";
     document.getElementById("consoleArea").innerHTML += "<br><span class=\"dollar\">$ </span>";
     introDone = true;
-    document.removeEventListener("click",clickToClose);
+    document.removeEventListener("click", clickToClose);
     slideshows[slideshow].count = -1;
     var dollars = document.getElementsByClassName("dollar");
-    dollars[dollars.length-1].scrollIntoView();
-    window.setTimeout(function(){
-      document.getElementById(slideshow+"Content").innerHTML = slideshows[slideshow].originalContent;
-    },500);
+    dollars[dollars.length - 1].scrollIntoView();
+    window.setTimeout(function () {
+      document.getElementById(slideshow + "Content").innerHTML = slideshows[slideshow].originalContent;
+    }, 500);
   } else if (slideshows[slideshow].count == 0) {
-    slideshows[slideshow].originalContent = document.getElementById(slideshow+"Content").innerHTML;
-    document.getElementById(slideshow+"Content").innerHTML = slideshows[slideshow].content[slideshows[slideshow].count];
+    slideshows[slideshow].originalContent = document.getElementById(slideshow + "Content").innerHTML;
+    document.getElementById(slideshow + "Content").innerHTML = slideshows[slideshow].content[slideshows[slideshow].count];
     document.getElementsByClassName("separateWindowActive")[0].children[0].children[0].children[0].scrollIntoView();
   } else {
-    document.getElementById(slideshow+"Content").innerHTML = slideshows[slideshow].content[slideshows[slideshow].count];
+    document.getElementById(slideshow + "Content").innerHTML = slideshows[slideshow].content[slideshows[slideshow].count];
     document.getElementsByClassName("separateWindowActive")[0].children[0].children[0].children[0].scrollIntoView();
   }
 }
